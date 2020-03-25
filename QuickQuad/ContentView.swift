e2e46ad368219da355d22a72f82421ee4b1850ac
Toyoshin <toyodasinngo@gmail.com>
2020-03-25 16:20:38 +0900

    Initial Commit

---
+++ QuickQuad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickQuad
+//
+//  Created by Toyoshin on 2020/03/25.
+//  Copyright © 2020 Toyoshin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickQuadTests/QuickQuadTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuickQuadTests.swift
+//  QuickQuadTests
+//
+//  Created by Toyoshin on 2020/03/25.
+//  Copyright © 2020 Toyoshin. All rights reserved.
+//
+
+import XCTest
+@testable import QuickQuad
+
+class QuickQuadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
